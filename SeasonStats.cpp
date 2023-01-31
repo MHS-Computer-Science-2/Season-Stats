d23cbf5441b326e5419f28190851ef2b59e3933c
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-01-31 16:00:41 -0500

    Create SeasonStats.cpp

---
+++ SeasonStats.cpp
@@ -0,0 +1,18 @@
+// Season Stats
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    //Create an input file variable
+    ifstream fin;
+    //Open the file
+    fin.open("scores.txt"); //make sure you create this file!
+
+  
+
+
+
+    system("pause");
+}
